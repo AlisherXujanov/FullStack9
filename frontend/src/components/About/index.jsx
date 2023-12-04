d2fda067ad9c552968047fe4658fcc6dfb4bb7dc
Alisher <alisherxujanov163@gmail.com>
2023-12-04 17:36:19 +0500

    created toogleTheme

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react'
+
 function About(props) {
+    const [theme, setTheme] = useState(false)
+
+    function handleTheme(e) {
+        try {
+            setTheme(!theme)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    const style = {
+        backgroundColor: theme ? "#333" : "#FFF",
+        color: theme ? "#FFF" : "#333"
+    }
+
     return (
-        <div>
-           <h1>{ props.title }</h1>
-           { props.children }
+        <div style={style}>
+            <h1>{props.title}</h1>
+            {props.children}
+
+            <hr />
+            <h3>{theme ? "Dark theme" : "Light theme"}</h3>
+            <button onClick={handleTheme}>
+                Toggle Theme
+            </button>
         </div>
     );
 }
