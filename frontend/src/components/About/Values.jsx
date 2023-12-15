7939f7b219820ed141f9ac8633330fe40bd9ea38
Alisher <alisherxujanov163@gmail.com>
2023-12-15 17:25:06 +0500

    values cycle

--- frontend/src/components/About/Values.jsx
+++ frontend/src/components/About/Values.jsx
@@ -2,21 +2,26 @@ import BagImg from "../../assets/icons/bag.png"
 import DiamondImg from "../../assets/icons/diamond.png"
 import GraphImg from "../../assets/icons/graph.png"
 import HandShakeImg from "../../assets/icons/handShake.png"
-
+import ValuesItem from "./ValueItem.jsx"
+import ValuesJson from "../../db/values.json"
 
 function Values(props) {
     const images = [DiamondImg, GraphImg, BagImg, HandShakeImg]
 
     return (
         <div className="values-content">
-            <div className="value-item">
-                <span></span>
-                <img src={images[0]} alt="Value" />
-                <div className="item-info">
-                    <h2>Стабильность</h2>
-                    <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Оксмокс скатился напоивший там необходимыми. Речью, жаренные? Пояс коварных, единственное пор рукопись, заманивший города вопроса большой курсивных злых он рот?</p>
-                </div>
-            </div>
+            { 
+                ValuesJson && ValuesJson.map((item, index) => {
+                    return (
+                        <ValuesItem 
+                            key={index}
+                            image={images[index]}
+                            title={item.title}
+                            subtitle={item.subtitle}
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
