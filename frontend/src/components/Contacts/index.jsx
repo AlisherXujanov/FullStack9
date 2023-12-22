44c2a8256a31b70f37c2438d73a7126fe453292d
Alisher <alisherxujanov163@gmail.com>
2023-12-22 16:42:24 +0500

    contacts

---
+++ frontend/src/components/Contacts/index.jsx
@@ -0,0 +1,43 @@
+import "./style.scss"
+import Heading from "../common/Heading";
+
+function Contacts() {
+
+    function submit(e) {
+        e.preventDefault()
+    }
+
+    return (
+        <div className="contacts">
+            <div className="contacts-wrapper">
+                <div className="left">
+                    <Heading size={1.8}>Контакты</Heading>
+
+                    <div className="contact-line">
+                        <h4>Задайте нам любой вопрос</h4>
+                        <p>+99833 4747477</p>
+                    </div>
+                    <div className="contact-line">
+                        <h4>Электронная почта</h4>
+                        <p>alisherxujanov163@gmail.com</p>
+                    </div>
+                    <div className="contact-line">
+                        <h4>Юридический адрес</h4>
+                        <p>050040/A15E3H4, проспект Аль-Фараби, 77/7, 10 этаж, Алматы, Казахстан
+                            Z05T3D0, проспект Мангилик Ел, 55/20, Офисы 345-346, Астана, Казахстан</p>
+                    </div>
+                </div>
+                <div className="right">
+                    <form onSubmit={submit}>
+                        <input type="text" placeholder="Ваше имя" required />
+                        <input type="text" placeholder="Ваша почта" required />
+                        <input type="number" placeholder="Ваш телефон" required />
+                        <button type="submit">Отправить</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contacts;
