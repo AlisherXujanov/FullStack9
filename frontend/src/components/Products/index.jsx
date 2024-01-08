f5aa082b650bc837e4700ce2be4a1a7a18c084ce
Alisher <alisherxujanov163@gmail.com>
2024-01-08 15:24:17 +0500

    useState usage

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,13 +1,30 @@
 import './style.scss'
+import { useState } from 'react'
 // RULES OF HOOKS
 // 1. Always import hook from 'react'
 // 2. Always call it at the top level of the component
-
 function Products(props) {
-
+    const [counter, setCounter] = useState(0)
+    
     return (
         <div id="products-wrapper">
-            <h2>Products</h2>
+            <h2>{counter}</h2>
+            <br />
+            {/* 
+                function(e) {
+                    setCounter(...)
+                }
+            */}
+            <button className='warning-btn' 
+                onClick={(e) => {setCounter(counter+2)}}
+            >
+                Increment
+            </button>
+            <button className='warning-btn' 
+                onClick={(e) => {setCounter(counter-2)}}
+            >
+                Decrement
+            </button>
         </div>
     );
 }
