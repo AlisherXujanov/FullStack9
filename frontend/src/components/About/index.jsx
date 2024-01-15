7cd87d8ce21e3ed85e43ffa8b01ce08087bd6ea3
Alisher <alisherxujanov163@gmail.com>
2024-01-15 14:47:35 +0500

    Heading component

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,11 +1,22 @@
 import './style.scss'
 
-
+function Heading(props) {
+    // props = {
+    //     children: "..." ,
+    //     size: 2,
+    // }
+    const size = `${48 / props.size}px`
+    return (
+        <h1 style={{fontSize: size}}>{props.children}</h1>
+    )
+}
 function About() {
     return (
-        <>
-            <h3>About</h3>
-        </>
+        <div id='about-page'>
+            <Heading size={2}>
+                About page heading
+            </Heading>
+        </div>
     );
 }
 
