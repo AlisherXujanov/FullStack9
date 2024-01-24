adc9fb7acd96cb75d0bda816361265ba9656c389
Alisher <alisherxujanov163@gmail.com>
2024-01-24 15:45:57 +0500

    scrollTo top in blogDetails

--- frontend/src/components/Blog/BlogDetails.jsx
+++ frontend/src/components/Blog/BlogDetails.jsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
 import BlogItems from '../../db/blog.json'
 import "./blogDetails.scss"
 
@@ -10,11 +11,18 @@ import Blog5 from "../../assets/images/img3.png"
 
 function BlogDetails(props) {
     const { id } = useParams()
-    const item = BlogItems.find(item => item.id === parseInt(id))
 
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }, [])
+
+    const item = BlogItems.find(item => item.id === parseInt(id))
     const images = [Blog1, Blog2, Blog3, Blog4, Blog5]
     let imgIndex = null
-    if (item.id < images.length) { imgIndex = item.id } 
+    if (item.id < images.length) { imgIndex = item.id }
     else { imgIndex = item.id % images.length }
 
     const subtitle = item.title.split(" ").slice(0, 3).join(" ")
@@ -22,20 +30,19 @@ function BlogDetails(props) {
     return (
         <div className="blog-details-wrapper">
             <p className="nav-subtitle">
-                Blog / { subtitle }
+                Blog / {subtitle}
             </p>
             <h1>{item.title}</h1>
 
-            <p className="author">
+            <h3 className="author">
                 <span className="name">{item.author},</span>
                 <span className="date">{item.date}</span>
-            </p>
-            <img 
-                src={images[imgIndex]} 
+            </h3>
+            <img
+                src={images[imgIndex]}
                 alt={"Blog-" + item.id}
                 className={"blog-" + item.id}
-                width={"100%"}
-                height={300}
+                height={400}
             />
             <p className="subtitle-1">{item.subtitle1}</p>
             <p className="subtitle-2">{item.subtitle2}</p>
