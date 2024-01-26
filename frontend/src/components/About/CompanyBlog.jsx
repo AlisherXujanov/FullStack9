4e459cd92d9483092f2455e970da547b3caf4ef2
Alisher <alisherxujanov163@gmail.com>
2024-01-26 15:38:10 +0500

    pagination @media

--- frontend/src/components/About/CompanyBlog.jsx
+++ frontend/src/components/About/CompanyBlog.jsx
@@ -1,12 +1,20 @@
 import Pagination from "../common/Pagination"
 import BlogItems from "../../db/blog.json"
 
+
 function CompanyBlog(props) {
+    let itemsPerPage = 3
+
+    const largeScreen = 1100
+    const mediumScreen = 700
+    if (window.innerWidth < largeScreen) { itemsPerPage = 2 }
+    if (window.innerWidth < mediumScreen) { itemsPerPage = 1 }
+
     return (
         <>
             <Pagination
                 items={BlogItems}
-                itemsPerPage={3}
+                itemsPerPage={itemsPerPage}
                 layoutName={"BlogLayout"}
             />
         </>
--- frontend/src/components/common/Pagination/index.jsx
+++ frontend/src/components/common/Pagination/index.jsx
@@ -34,16 +34,12 @@ function PaginatedItems(props) {
     // (This could be items from props; or items loaded in a local state
     // from an API endpoint with useEffect and useState)
     const endOffset = itemOffset + props.itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
     const currentItems = props.items.slice(itemOffset, endOffset);
     const pageCount = Math.ceil(props.items.length / props.itemsPerPage);
 
     // Invoke when user click to request another page.
     const handlePageClick = (event) => {
         const newOffset = (event.selected * props.itemsPerPage) % props.items.length;
-        console.log(
-            `User requested page number ${event.selected}, which is offset ${newOffset}`
-        );
         setItemOffset(newOffset);
     };
 
