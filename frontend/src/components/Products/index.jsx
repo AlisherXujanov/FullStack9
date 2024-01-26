15b6de116dca1f18b51ec48e5d9841850ea435d6
Alisher <alisherxujanov163@gmail.com>
2024-01-26 15:58:31 +0500

    useState started learning

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,9 +1,27 @@
 import './style.scss'
+import { useState } from "react"
+
 
 function Products(props) {
+    const [count, setCount] = useState(10)
+
+    function inc(e) {  setCount(count+1)  }
+
+    function dec(e) {  setCount(count-1)  }
+
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
+
+            <h1>{count}</h1>
+            <br />
+            <br />
+            <button className="warning-btn" onClick={inc}>
+                Increment
+            </button>
+            <button className="warning-btn" onClick={dec}>
+                Decrement
+            </button>
         </div>
     );
 }
