8ce5622c5d55e267c919e8e33ff4931c80910718
Alisher <alisherxujanov163@gmail.com>
2024-01-31 15:14:23 +0500

    handleDispatch

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,41 +1,86 @@
 import './style.scss'
-import { useState } from "react"
+import { useReducer } from "react"
+
+const initialState = { // начальное состояние
+    count: 10,
+    text: "",
+    color: "red",
+}
+
+function reducerFunction(state, action) {
+    // {type: 'increment'}  == parameter
+    // parameter.type   ==  'increment'
+    switch (action.type) {
+        case 'increment':
+            return { ...state, count: state.count+1 }
+        case 'decrement':
+            return { ...state, count: state.count-1 }
+        case 'color':
+            return { ...state, color: action.value }
+        case 'text':
+            return { ...state, text: action.value }
+        default:
+            return state
+    }
+    // RULE №1
+    // ALWAYS, the item that is returned from this function becomes the new state
+    // RU: ВСЕГДА, то, что возвращается из этой функции становится новым состоянием
+}
 
 function Products(props) {
-    const [count, setCount] = useState(10)
-    const [text, setText] = useState("")
-    const [color, setColor] = useState("red")
+    const [state, dispatch] = useReducer(reducerFunction, initialState)
+    // state    === initialState
+    // dispatch === reducerFunction
 
-    function inc(e) { setCount(count + 1) }
-    function dec(e) { setCount(count - 1) }
+    // const parameter = {type: 'increment'}
+    // dispatch(parameter)
+
+    // const [count, setCount] = useState(10)
+    // const [text, setText] = useState("")
+    // const [color, setColor] = useState("red")
+
+    // function inc(e) { setCount(count + 1) }
+    // function dec(e) { setCount(count - 1) }
+
+    function handleDispatch(e) {
+        const tag_name = e.target.name
+    }
 
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
 
             <div className='container'>
-                <h1>{count}</h1>
+                <h1>{state.count}</h1>
                 <br />
                 <br />
-                <button className="warning-btn" onClick={inc}>
+                <button className="warning-btn" name='increment' 
+                    onClick={()=> {dispatch({type:"increment"}) }}
+                >
                     Increment
                 </button>
-                <button className="warning-btn" onClick={dec}>
+                <button className="warning-btn" name='decrement' 
+                    onClick={() => {dispatch({type:"decrement"}) }}
+                >
                     Decrement
                 </button>
             </div>
 
             <div className="container">
-                <h3>{text}</h3>
-                <input type="text" 
-                    onChange={(e) => {setText(e.target.value)}} 
+                <h3>{state.text}</h3>
+                <input type="text" name='text'
+                    onChange={(e) => { dispatch({type:"text", value:e.target.value}) }} 
                 />
             </div>
 
-            <div className="container" style={{backgroundColor: color}}>
-                <input type="color" onChange={(e) => setColor(e.target.value)} />
+            <div className="container" style={{backgroundColor: state.color}}>
+                <input type="color" name='color'
+                    onChange={(e) => { dispatch({type:"color", value:e.target.value}) }} 
+                />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis id ipsam temporibus quo laborum. Commodi neque facere dicta nihil, alias perspiciatis, quis eos fugiat explicabo, dolor enim eaque ex tenetur?</p>
             </div>
+
+
         </div>
     );
 }
