571c5b6712be43e7c52e794a1dde5b7fc4eb7710
Alisher <alisherxujanov163@gmail.com>
2024-02-02 15:33:18 +0500

    example of using global context

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,10 +1,20 @@
 import './style.scss'
+import { context } from '../../store'
+import { useContext } from 'react'
 
 
 function Products(props) {
+    const { store, setStore } = useContext(context)
+
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
+            <h3>{store.counter}</h3>
+            <button className="warning-btn"
+                onClick={() => setStore({type:'increment'})}
+            >
+                Increment
+            </button>
         </div>
     );
 }
--- frontend/src/store/helpers.js
+++ frontend/src/store/helpers.js
@@ -2,6 +2,8 @@ function globalReducer(state, action) {
     switch (action.type) {
         case 'increment':
             return { ...state, counter: state.counter + 1 }
+        case 'decrement':
+            return { ...state, counter: state.counter - 1 }
         default:
             return state
     }
