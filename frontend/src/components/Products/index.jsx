d72c56bf850a1a3d8508958d182054747b4e64c7
Alisher <alisherxujanov163@gmail.com>
2024-02-05 15:49:11 +0500

    rhombus-spinner

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,5 +1,5 @@
 import './style.scss'
-import './spinner.scss'
+import Spinner from "../common/Spinner"
 import { context } from '../../store'
 import { useContext, useEffect } from 'react'
 import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
@@ -67,17 +67,7 @@ function Products(props) {
                         else if (isLoading) {
                             return (
                                 <div>
-                                    <div class="breeding-rhombus-spinner">
-                                        <div class="rhombus child-1"></div>
-                                        <div class="rhombus child-2"></div>
-                                        <div class="rhombus child-3"></div>
-                                        <div class="rhombus child-4"></div>
-                                        <div class="rhombus child-5"></div>
-                                        <div class="rhombus child-6"></div>
-                                        <div class="rhombus child-7"></div>
-                                        <div class="rhombus child-8"></div>
-                                        <div class="rhombus big"></div>
-                                    </div>
+                                    
                                 </div>
                             )
                         }
---
+++ frontend/src/components/common/Spinner/index.jsx
@@ -0,0 +1,19 @@
+import "./style.scss"
+function Spinner() {
+    return (
+        <>
+            <div class="breeding-rhombus-spinner">
+                <div class="rhombus child-1"></div>
+                <div class="rhombus child-2"></div>
+                <div class="rhombus child-3"></div>
+                <div class="rhombus child-4"></div>
+                <div class="rhombus child-5"></div>
+                <div class="rhombus child-6"></div>
+                <div class="rhombus child-7"></div>
+                <div class="rhombus child-8"></div>
+                <div class="rhombus big"></div>
+            </div>
+        </>
+    );
+}
+export default Spinner;
