6860bed328ab6fe5cfd0c27207bb790d0c511d22
Alisher <alisherxujanov163@gmail.com>
2024-02-12 14:34:17 +0500

    reg form

--- frontend/src/components/Authentication/Registration.jsx
+++ frontend/src/components/Authentication/Registration.jsx
@@ -1,11 +1,20 @@
 import Heading from '../common/Heading'
 import "./authContent.scss"
+import { useState } from 'react'
 
 function Registration(props) {
+    const [regState, setRegState] = useState({})
 
     function submit(e) {
         e.preventDefault();
     }
+
+    function handleState(e) {
+        const key = e.target.name
+        const val = e.target.value
+        setRegState({ ...regState, [key]: val })
+    }
+
     return (
         <div className="auth-content-wrapper">
             <Heading size={2}>Registration</Heading>
@@ -13,19 +22,27 @@ function Registration(props) {
             <form onSubmit={submit}>
                 <div className="form-control">
                     <label htmlFor="name">Username</label>
-                    <input type="name" id="name" placeholder='Username' />
+                    <input type="name" id="name" placeholder='Username' name='username'
+                        onChange={handleState}
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" placeholder='Email' />
+                    <input type="email" id="email" placeholder='Email' name='email'
+                        onChange={handleState}
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" placeholder='Password' />
+                    <input type="password" id="password" placeholder='Password' name='password1'
+                        onChange={handleState}
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="password-conf">Password confirmation</label>
-                    <input type="password" id="password-conf" placeholder='Password confirmation' />
+                    <input type="password" id="password-conf" placeholder='Password confirmation' name='password2'
+                        onChange={handleState}
+                    />
                 </div>
                 <div className="form-control">
                     <button className='warning-btn'>
